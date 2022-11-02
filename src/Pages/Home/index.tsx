2af1aecee20366afc8af44f57b0649e1ee1ce5eb
Eduardo Rangel <eduardo_rp23@hotmail.com>
2022-11-02 13:33:44 -0300

    feature: created folders of initial settings and stylizations

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./style"
+
+export const Home = () =>{
+    return(
+        <Container>
+            ola
+        </Container>
+    )
+}
---
+++ src/Styles/GlobalStyle.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    :root{
+        --background: ##3D5A73;
+        --purple-black: #5A0E5C;
+        --purple-light: #A84088;
+        --white: #f5f5f5;
+        --blue: #98C1D9;
+        --blue-light: #E0FBFC;
+        --text: #8385D1;
+        --Poppins: font-family: 'Poppins', sans-serif;
+        --Roboto: font-family: 'Roboto Condensed', sans-serif;
+    }
+
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    ul,li{
+        list-style: none;
+    }
+
+    a{
+        text-decoration: none;
+    }
+
+    body, html{
+        font-size: 16px;
+    }
+`
