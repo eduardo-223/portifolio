e07c8af7a1af4a205562e6794a1940bde81c6b95
Eduardo Rangel <eduardo_rp23@hotmail.com>
2022-11-10 23:43:49 -0300

    feature: create menu wich animaticon text center

--- src/Components/HomeCp/index.tsx
+++ src/Components/HomeCp/index.tsx
@@ -1,12 +1,18 @@
 import { Container } from "./style";
 
+import Cartoon from "../../Assets/img/cartoonImg.jpg";
+
 import Typical from "react-typical";
 
 export const HomeCp = () => {
   return (
     <Container>
-      <Typical steps={[`Ola Mundo!`, 1500, "Hello World!", 1500]} loop={Infinity} />
+      <Typical
+        steps={[`Ola Mundo!`, 500, "Hello World!", 1000]}
+        loop={Infinity}
+      />
       <h2>Sou Eduardo, desenvolvedor Front End!!</h2>
+      <img src={Cartoon} alt="Profile Avatar" />
     </Container>
   );
 };
--- src/Components/HomeCp/style.ts
+++ src/Components/HomeCp/style.ts
@@ -5,9 +5,16 @@ export const Container = styled.main`
     font-weight: 900;
     color: white;
     font-size: 2.5rem;
+    padding: 0 15px;
 
     h2{
         margin-top: 20px;
         font-size: 1.8rem;
+    }
+
+    img{
+        width: 250px;
+        height: 250px;
+        border-radius: 50%;
     }
 `
